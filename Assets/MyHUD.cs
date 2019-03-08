6a288741aae198f5707aa09d752d94156971869a
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-09 00:40:00 +0530

    MyHostStart

---
+++ Assets/MyHUD.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHUD : MonoBehaviour {
+
+	private NetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+		networkManager = GetComponent<NetworkManager> ();
+	}
+
+	public void MyStartHost(){
+		Debug.Log (Time.timeSinceLevelLoad);
+		networkManager.StartHost ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnStartHost(){
+		Debug.Log (Time.timeSinceLevelLoad);
+	}
+}
