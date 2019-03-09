62b9a14b7fad398c274850b032c373772ee54a27
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-09 18:48:12 +0530

    MOverride methods

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void MyStartHost(){
+		Debug.Log (Time.timeSinceLevelLoad);
+		StartHost ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void OnStartHost(){
+		Debug.Log (Time.timeSinceLevelLoad);
+	}
+}
